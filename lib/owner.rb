37a6b6159cfe629017a4cbfbd88f0b673027350c
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-17 03:26:58 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -53,4 +53,10 @@ class Owner
       @pets[:fishes].each {|fish| fish.mood="happy"}
     end
 
+    def sell_pets
+      @pets[:cats].each {|cat| cat.mood="nervous"}
+      @pets[:fishes].each {|fish| fish.mood="nervous"}
+      @pets[:dogs].each {|dog| dog.mood="nervous"}
+    end
+
 end
